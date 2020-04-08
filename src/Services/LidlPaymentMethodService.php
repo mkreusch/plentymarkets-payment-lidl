0e16b7703b6a89c1734b38cb61bfaa104d1dab8d
mkreusch <info@kreusch-creative.de>
2020-04-08 13:01:46 +0200

    v1.0.2

--- src/Services/LidlPaymentMethodService.php
+++ src/Services/LidlPaymentMethodService.php
@@ -8,6 +8,7 @@ class LidlPaymentMethodService extends PaymentMethodService
 {
     const PLUGIN_KEY = 'plenty_LidlPayment';
     const PAYMENT_KEY = 'LidlPayment';
+    const PAYMENT_NAME = 'Lidl';
 
     public function isBackendSearchable()
     {
@@ -19,7 +20,18 @@ class LidlPaymentMethodService extends PaymentMethodService
         return true;
     }
 
-    public function isActive(){
+    public function isActive()
+    {
+        return true;
+    }
+
+    public function getBackendName(string $lang)
+    {
+        return self::PAYMENT_NAME;
+    }
+
+    public function canHandleSubscriptions()
+    {
         return true;
     }
 }
